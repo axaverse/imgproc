dbf8407e1f497c9734e3d6619549a1d97f711467
axaverse <aaqsaaminn@gmail.com>
2025-10-13 21:40:27 +0500

    Trial run invert filter

--- test/test_runner.c
+++ test/test_runner.c
@@ -2,6 +2,8 @@
 
 #include "io.h"
 #include "types.h"
+#include "process/pixel_ops.h"
+#include "process/process.h"
 
 void dump_image(const Image* img) {
     if (!img) { return; }
@@ -40,5 +42,41 @@ int main() {
 
     dump_image(&img);
     free_image(&img);
+
+    // -------------------------------------------------
+    // test_images/bg2.png
+
+    int err = load_image(&img, "test_images/bg2.png");
+    if (err) {
+        printf("Failed to load image: %d\n", err);
+        return err;
+    }
+
+    // dump_image(&img);
+
+    Image out;
+    out.channels = img.channels;
+    out.m = img.m;
+    out.n = img.n;
+    out.format = img.format;
+
+    out.data = (int32_t *)malloc(out.m * out.n * out.channels * sizeof(int32_t));
+
+    err = map_image(&out, &img, invert, NULL);
+
+    if (err) {
+        printf("Failed to process image: %d\n", err);
+        free_image(&img);
+        free_image(&out);
+        return err;
+    }
+
+    // dump_image(&out);
+
+    store_image("test_images/out_invert.png", &out);
+
+    free_image(&out);
+    free_image(&img);
+
     return 0;
 }
